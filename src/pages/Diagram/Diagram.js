dff9b8fd13e9db416c81f6781e17f535f5c7465a
Шушаков Александр <kolyasashatwo@gmail.com>
2024-12-22 18:41:35 +0700

    API block

--- src/pages/Diagram/Diagram.js
+++ src/pages/Diagram/Diagram.js
@@ -2,6 +2,8 @@ import React, {useEffect, useRef, useState} from 'react';
 import * as go from 'gojs';
 import saveBlock from "./Blocks/saveBlock";
 import messageBlock from "./Blocks/messageBlock";
+import apiBlock from "./Blocks/apiBlock";
+
 import {createConditionalBlock} from "./Blocks/conditionalBlock";
 import {createOptionsBlock} from "./Blocks/optionsBlock";
 import createPort from "./Blocks/createPort";
@@ -88,6 +90,7 @@ const Diagram = () => {
     }
 
     diagram.nodeTemplateMap.add("saveBlock", saveBlock);
+    diagram.nodeTemplateMap.add("apiBlock", apiBlock);
     diagram.nodeTemplateMap.add("messageBlock", messageBlock);
     diagram.nodeTemplateMap.add("conditionalBlock", createConditionalBlock(diagram));
     diagram.nodeTemplateMap.add("optionsBlock", createOptionsBlock(diagram));
@@ -158,6 +161,12 @@ const Diagram = () => {
           category: "saveBlock",
           name: "name",
         },
+        {
+          key: 5,
+          category: "apiBlock",
+          variable: "variable",
+          link: "https://api.blockchain.org",
+        },
       ],
       linkDataArray: [],
     });
@@ -197,6 +206,12 @@ const Diagram = () => {
         key: 4,
         category: "saveBlock",
         name: "name",
+      },
+      {
+        key: 5,
+        category: "apiBlock",
+        variable: "variable",
+        link: "https://api.blockchain.org",
       },
     ]);
     diagramRefObject.current = diagram;
@@ -272,7 +287,7 @@ const Diagram = () => {
           ref={paletteRef}
           style={{
             background: "rgb(10,10,10)",
-            width: "150px",
+            width: "250px",
             height: "100vh",
             borderRight: "1px dashed white",
             borderRadius: 10,
